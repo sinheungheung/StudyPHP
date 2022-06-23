530f8fce94cce25a8db52c00d2ee40ac24ddfbf2
Heung <80873447+sinheungheung@users.noreply.github.com>
2022-06-24 01:27:56 +0900

    form 태그

---
+++ 3.php
@@ -0,0 +1,24 @@
+<?php
+// html안에 php를 삽입할 때는 echo 코드;
+// php안에 html을 삽입할 때는 echo "<h1><br/>"; 
+
+// form 태그: 사용자 입력을 받는 태그
+// 1. input태그
+// 1) text: 한 줄 입력
+// 2) password: ***으로 입력
+// 3) checkbox: 여러 개 선택
+// 4) radio: 한 개 선택
+// 5) submit: 제출전송
+// 6) reset: 취소
+// 7) email: 이메일 입력
+// 8) number: 숫자 입력
+// 9) color: 컬러picker
+// 10) file: 파일 선택
+
+// value="kim" maxlength="10"생략가능
+// select: 콤보박스
+// textarea: 여러 줄 입력
+
+// 결과창에 경고 내보내기
+// echo "<script>alert('비밀번호가 일치하지 않습니다');history.go(-1);">
+?>
